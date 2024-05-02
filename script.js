7185ab818f341aa95b3e5a11a2abd019e96bceba
isabellagoes <isabellagoes.igs@gmail.com>
2024-05-02 20:33:22 -0300

    .

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ console.log(window.navigator.userAgent);
  
 console.log(window.screen.width);
 console.log(window.screen.height); */
+
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+
+console.log(document.body.children);
+
+let titulo = document.body.children[0];
+console.log(titulo);
+
+let lista = document.body.children[1];
+console.log(lista);
+
+console.log(lista.firstElementChild);
+console.log(lista.lastElementChild);
+console.log(lista.parentElement);
+
+console.log(document.getElementById("listinha"));
+console.log(document.getElementById("item01"));
+
+console.log(document.querySelectorAll(".ordenada"));
+console.log(document.querySelectorAll("#item01"));
