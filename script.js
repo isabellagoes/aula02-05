3d4ce3055394b497eadfac2dd10a0d8c16595ff3
isabellagoes <isabellagoes.igs@gmail.com>
2024-05-02 20:06:09 -0300

    primeira parte

---
+++ script.js
@@ -0,0 +1,10 @@
+/* console.log("Funciona");
+ 
+window.console.log("Pelo Window");
+console.log(window.location.href);
+console.log(window.navigator.language);
+console.log(window.navigator.platform);
+console.log(window.navigator.userAgent);
+ 
+console.log(window.screen.width);
+console.log(window.screen.height); */
